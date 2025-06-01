e5ac253221e03076ee5bde9c86285885b7b3b8d0
josephKaguong <josephkaguongo8@gmail.com>
2025-06-01 19:29:09 +0300

    a-redirect

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,36 @@
+// Rest of the import statements
+ import { Poppins_400Regular } from '@expo-google-fonts/poppins/400Regular';
 import { useFonts } from 'expo-font';
-import { Stack } from "expo-router";
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import '../global.css';
 
-SplashScreen.preventAutoHideAsync()
+import { Stack } from 'expo-router';
+
+
+import * as SplashScreen from 'expo-splash-screen';
+
+import { useEffect } from 'react';
+
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function RootLayout() {
-  
-  const [loaded,error]=useFonts({
-    'poppins':require('typeface-poppins')
-  })
+  const [loaded, error] = useFonts({
+    
+    Poppins_400Regular,
+    
+  });
 
-  useEffect(()=>{
-       if(loaded){
-       SplashScreen.hideAsync()
-  }
-  },[loaded,error])
- 
-  if(!loaded || !error){
-    return null
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)/home"/>
-    </Stack>
-
-  ) 
+    <Stack/>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
@@ -18,9 +21,9 @@ module.exports = {
           light:"#FFFFFF"
         }
       },
-      fontFamily:{
-        Poppins:'poppins'
-      }
+     fontFamily:{
+      poppins:['Poppins_400Regular']
+     }
     },
   },
   plugins: [],
